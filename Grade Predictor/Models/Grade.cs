5b09c0ceb8d6d36b2560dd5677d5f69683e9ac8a
hovtoros <8dasaran@gmail.com>
2016-04-26 23:10:23 +0400

    The project files.

---
+++ Grade Predictor/Models/Grade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Predictor.Models
+{
+    class Grade
+    {
+
+    }
+}
